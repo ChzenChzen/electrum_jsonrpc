1ff997bd49d7e5de76168c6e001a07d510fa524a
fomotoshi <fomotoshi@ya.ru>
2021-02-16 02:15:02 +0300

    error.rs: minor fixes

--- src/error.rs
+++ src/error.rs
@@ -2,37 +2,39 @@ use std::net::AddrParseError;
 use std::fmt;
 use std::error;
 use crate::error::ElectrumRpcError::AddressError;
+use std::fmt::Display;
+use hyper::http::uri::InvalidUri;
 
 pub type Result<T> = std::result::Result<T, ElectrumRpcError>;
 
 pub enum ElectrumRpcError {
-    AddressError(AddrParseError),
+    AddressError(InvalidUri),
 }
 
 impl fmt::Display for ElectrumRpcError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            AddressError(..) => write!(f, "the provided address couldn't parsed."),
+            AddressError(_) => write!(f, "the provided address couldn't parsed."),
         }
     }
 }
 
 impl fmt::Debug for ElectrumRpcError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        <ElectrumRpcError as std::fmt::Display>::fmt(self, f)
+        <Self as fmt::Display>::fmt(self, f)
     }
 }
 
 impl error::Error for ElectrumRpcError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match *self {
-            AddressError(ref e) => e.source(),
+        match &*self {
+            AddressError(ref e) => Some(e),
         }
     }
 }
 
-impl From<AddrParseError> for ElectrumRpcError {
-    fn from(err: AddrParseError) -> Self {
+impl From<InvalidUri> for ElectrumRpcError {
+    fn from(err: InvalidUri) -> Self {
         Self::AddressError(err)
     }
 }
