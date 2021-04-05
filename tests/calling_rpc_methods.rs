ff0e2ce56cc7500e1912299be40f13a3090a1860
fomotoshi <fomotoshi@ya.ru>
2021-04-05 21:46:48 +0300

    calling_rpc_methods.rs: added `call_method_pay_to`

--- tests/calling_rpc_methods.rs
+++ tests/calling_rpc_methods.rs
@@ -1,11 +1,15 @@
 //! Integration tests for Electrum's json-rpc calls.
 
+use std::path::PathBuf;
+
+use hyper::{body, Uri};
+use rust_decimal::Decimal;
+use rust_decimal::prelude::FromPrimitive;
+use serde_json::Value;
+use tokio;
+
 use electrum_jsonrpc::btc::BtcAddress;
 use electrum_jsonrpc::ext::tests::*;
-use hyper::{body, Uri};
-use serde_json::Value;
-use std::path::PathBuf;
-use tokio;
 
 #[tokio::test]
 async fn call_method_help() {
@@ -123,4 +127,17 @@ async fn call_method_close_wallet() {
 
     let json: Value = serde_json::from_slice(&slice).unwrap();
     assert_eq!(json["result"], true, "\njson body is: {}", json);
+}
+
+#[tokio::test]
+async fn call_method_pay_to() {
+    let electrum = get_electrum_rpc();
+    let addr = BtcAddress::new("tb1qncyt0k7dr2kspmrg3znqu4k808c09k385v38dn");
+    let amount = Decimal::from_f64(0.00001).unwrap();
+    let res = electrum.pay_to(&addr, amount, None).await.unwrap();
+    let slice = body::to_bytes(res).await.unwrap();
+
+    let json: Value = serde_json::from_slice(&slice).unwrap();
+    let expected = "02000000000101b58c5be9c9ce77a8bacd01779fdcfbf566a936a5b89482d1bc3114525ee5f3ea0000000000fdffffff02e8030000000000001600149e08b7dbcd1aad00ec6888a60e56c779f0f2da276022000000000000160014d272035ef819d6311231c06014aed5cfb100009e0247304402203db69d69b3fa76050b6c3276bc21bb834996f2c84c31c17c813beba01079705002202d864669f12db9939ea78a45e4c4a982cca68304fef25f334bd6cbbc9971bc9b012103815054ce939185772574ef569fe31b601d5bad48f48d5edaef194cded838c31ac40f1e00";
+    assert_eq!(json["result"], expected, "\njson body is: {}", json);
 }
